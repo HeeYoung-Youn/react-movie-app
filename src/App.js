e4eb4dd8cb5ef170c969c3b9c5ac4e9768811a47
HeeYoung-Youn <yhy1@naver.com>
2020-09-12 22:34:18 +0900

    Object로 전달한 Props 사용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
 
-function Movie({name}) {
+function Movie({name, picture}) {
   return (
-  <h1>I like {name}</h1>
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
   );
 }
+
+const movieILike = [
+  {
+    name: 'iron man',
+    image: 'https://upload.wikimedia.org/wikipedia/en/0/00/Iron_Man_poster.jpg'
+  },
+  {
+    name: 'lala land',
+    image: 'https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png'
+  },
+  {
+    name: 'avengers',
+    image: 'https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg'
+  },
+  {
+    name: 'spider man',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71bD1oNkYnL._AC_SY879_.jpg'
+  },
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Movie name="iron man"/>
-      <Movie name="lala land"/>
-      <Movie name="avangers"/>
-      <Movie name="spider man"/>
+      {movieILike.map(movies => (
+        <Movie name={movies.name} picture={movies.image} />
+      ))}
     </div>
   );
 }
