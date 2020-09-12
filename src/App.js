a5d5b25b4cd2f7d94f16f15c39794d38b1bc3751
HeeYoung-Youn <yhy1@naver.com>
2020-09-12 22:11:27 +0900

    Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+function Movie({name}) {
+  return (
+  <h1>I like {name}</h1>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <Movie name="iron man"/>
+      <Movie name="lala land"/>
+      <Movie name="avangers"/>
+      <Movie name="spider man"/>
     </div>
   );
 }
