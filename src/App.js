84fa48afddd54c94e1e25c2880ac5d0b7d6b4bff
HeeYoung-Youn <yhy1@naver.com>
2020-09-13 02:19:11 +0900

    Functional Compnent -> Class Component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const movieILike = [
-  {
-    id: 1,
-    name: 'iron man',
-    image: 'https://upload.wikimedia.org/wikipedia/en/0/00/Iron_Man_poster.jpg',
-    rating: 4.9
-  },
-  {
-    id: 2,
-    name: 'lala land',
-    image: 'https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png',
-    rating: 4.6
-  },
-  {
-    id: 3,
-    name: 'avengers',
-    image: 'https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg',
-    rating: 4.5
-  },
-  {
-    id: 4,
-    name: 'spider man',
-    image: 'https://images-na.ssl-images-amazon.com/images/I/71bD1oNkYnL._AC_SY879_.jpg',
-    rating: 4.2
-  },
-]
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-function Movie({name, picture, rating}) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+  add = () => {
+    console.log('add');
+  };
 
-  Movie.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number
-}
+  minus = () => {
+    console.log('minus');
+  };
 
-function App() {
-  return (
-    <div>
-      {movieILike.map(movies => (
-        <Movie
-          key={movies.id}
-          name={movies.name}
-          picture={movies.image}
-          rating={movies.rating}
-        />
-      ))}
-    </div>
-  );
+  render(){
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
