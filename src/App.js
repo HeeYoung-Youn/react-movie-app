aaa06261307ddae2d65a798b0f2528fff652fbe4
HeeYoung-Youn <yhy1@naver.com>
2020-09-12 22:53:02 +0900

    id 추가

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import React from 'react';
 
-function Movie({name, picture}) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
-
 const movieILike = [
   {
+    id: 1,
     name: 'iron man',
     image: 'https://upload.wikimedia.org/wikipedia/en/0/00/Iron_Man_poster.jpg'
   },
   {
+    id: 2,
     name: 'lala land',
     image: 'https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png'
   },
   {
+    id: 3,
     name: 'avengers',
     image: 'https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg'
   },
   {
+    id: 4,
     name: 'spider man',
     image: 'https://images-na.ssl-images-amazon.com/images/I/71bD1oNkYnL._AC_SY879_.jpg'
   },
 ]
 
+function Movie({name, picture}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       {movieILike.map(movies => (
-        <Movie name={movies.name} picture={movies.image} />
+        <Movie key={movies.id} name={movies.name} picture={movies.image} />
       ))}
     </div>
   );
